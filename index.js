4ad8b4fcdf8d886019dc14d24ded4491076fbb54
akainly <akainly.akai@gmail.com>
2025-01-25 18:28:09 +0500

    update

--- index.js
+++ index.js
@@ -7,14 +7,77 @@ addEventListener('mousemove', ({ clientX, clientY }) => {
 });
 
 ['mouseenter', 'mouseleave'].forEach(e => document.addEventListener(e, () => {
-    if (e === 'mouseleave') bg.removeAttribute('style');
-    bg.style.transition = 'transform .1s linear';
+    if (e === 'mouseleave') {
+        bg.style.removeProperty('--tx');
+        bg.style.removeProperty('--ty');
+        bg.style.transition = 'transform .1s linear';
+    }
     setTimeout(() => bg.style.transition = '', 100);
 }));
 
-// random bg
-var images = ['/bg1.jpg',  '/bg2.jpg', '/bg3.jpg', '/bg4.jpg', '/bg5.jpg', '/bg6.jpg', '/bg7.jpg'];
-
+var images = ['/bg1.jpg'];
 var randomImage = Math.floor(Math.random() * images.length);
+document.querySelector(':root').style.setProperty("--background", "url('/backgrounds/" + images[randomImage] + "')");
 
-document.querySelector(':root').style.setProperty("--background", "image-set(url('/backgrounds" + images[randomImage] + "'))");
+//tictic
+function showCard(cardName) {
+    const akaiyaCard = document.getElementById('akaiyaCard');
+    const xikassCard = document.getElementById('xikassCard');
+    const rzxCard = document.getElementById('rzxCard');
+    const mdCard = document.getElementById('mdCard');
+
+    if (cardName === 'akaiya') {
+        akaiyaCard.style.display = 'block';
+        xikassCard.style.display = 'none';
+    } else if (cardName === 'xikass') {
+        xikassCard.style.display = 'block';
+        akaiyaCard.style.display = 'none';
+    } else if (cardName === 'rzx') {
+        rzxCard.style.display = 'block';
+        mdCard.style.display = 'none';
+    } else if (cardName === 'md') {
+        mdCard.style.display = 'block';
+        rzxCard.style.display = 'none';
+    }
+}
+
+// time
+function updateTime() {
+    const timeElement = document.getElementById('time');
+
+    const now = new Date();
+
+    const options = { timeZone: 'Asia/Almaty', hour12: false };
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        ...options,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+    });
+
+    timeElement.textContent = formatter.format(now).replace(/:/g, '.');
+}
+
+setInterval(updateTime, 1000);
+
+updateTime();
+
+// data
+function updateDate() {
+    const dateElement = document.getElementById('date');
+
+    const now = new Date();
+
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0'); 
+    const year = now.getFullYear();
+
+    dateElement.textContent = `${day}.${month}.${year}`;
+}
+
+updateDate();
+
+//link
+function redirectToSite(url) {
+    window.location.href = url;
+}
